75056f7be2b2ee8474d5d4f5904aa5d9f304a88d
nix <nix@local>
2025-12-28 21:08:16 +0100

    improve test script

--- test/change-resolution.py
+++ test/change-resolution.py
@@ -1,12 +1,17 @@
 import pickle
 import argparse
+import os
 
 parser = argparse.ArgumentParser(description="Edit a job pickle file")
 parser.add_argument("pkl_path", type=str, help="Path to the pickle (.pkl) file")
 parser.add_argument("height", type=int, help="New Video output Height as an integer")
 args = parser.parse_args()
 
-job = pickle.load(args.pkl_path)
+with open(args.pkl_path, "rb") as f:
+    job = pickle.load(f)
 job["outputHeight"] = args.height
-pickle.dump(job, args.pkl_path)
+job["name"] = str(args.height) + "_" + job["name"]
+
+with open(f"{args.height}_" + str(os.path.basename(args.pkl_path)), "wb") as f:
+    pickle.dump(job, f)
 
