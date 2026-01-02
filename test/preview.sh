c195fc65c4921d232c660af43fcf3af9b23656e2
nix <nix@local>
2026-01-02 14:05:25 +0100

    add removebg

--- test/preview.sh
+++ test/preview.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Description: Create preview video with applied alpha mask
 
 ffmpeg -i "$1" -filter_complex "[0:v]split=8[base0][base1][base2][base3][base4][base5][scaled_src][size_ref];[size_ref]geq=r=0:g=0:b=0[BLACK];[scaled_src]scale=iw*0.4:ih*0.4[scaled];[BLACK]scale=iw*0.4:ih*0.4[black_scaled];[base0]crop=w=iw*0.2:h=ih*0.2:x=iw/2-iw*0.1:y=ih-ih*0.2[lt];[base1]crop=w=iw*0.2:h=ih*0.2:x=iw/2-iw*0.1:y=0[lb];[base2]crop=w=iw*0.1:h=ih*0.2:x=iw-iw*0.1:y=ih-ih*0.2[rtl];[base3]crop=w=iw*0.1:h=ih*0.2:x=iw-iw*0.1:y=0[rbl];[base4]crop=w=iw*0.1:h=ih*0.2:x=0:y=ih-ih*0.2[rtr];[base5]crop=w=iw*0.1:h=ih*0.2:x=0:y=0[rbr];[lt][lb]vstack=inputs=2[lhalf];[rtl][rtr]hstack=inputs=2[rtop];[rbl][rbr]hstack=inputs=2[rbottom];[rtop][rbottom]vstack=inputs=2[rhalf];[lhalf][rhalf]hstack=inputs=2[s_mask];[s_mask]scale=iw:ih,format=gray8[fullmask];[scaled]scale=iw:ih[scaledf];[scaledf][fullmask]alphamerge[vamasked];[black_scaled][vamasked]overlay=format=auto[outv]" -map "[outv]" -map "0:a?" -c:a copy "$1-preview.mp4"
 
---
+++ test/remove_bg.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Description Remove the background for an given AR video with existing alpha mask
+
+w=$(ffprobe -v error -select_streams v:0 -show_entries stream=width -of csv=p=0 "$1")
+h=$(ffprobe -v error -select_streams v:0 -show_entries stream=height -of csv=p=0 "$1")
+fps=$(ffprobe -v error -select_streams v:0 -show_entries stream=r_frame_rate -of csv=p=0 "$1")
+echo "Width: $w, Height: $h, fps: $fps"
+
+ffmpeg -i "$1" -i mask_eroded.png -r $fps -i mask.png -r $fps -filter_complex "[1:v]scale=w=$w:h=$h[mask];[2:v]scale=w=$w*0.4:h=$h*0.4[mask_outher];[0:v]split=8[base0][base1][base2][base3][base4][base5][size_ref][scaled_src];[size_ref]geq=r=128:g=128:b=128[BLACK];[scaled_src]scale=iw:ih[scaled];[BLACK]scale=iw:ih[black_scaled];[base0]crop=w=iw*0.2:h=ih*0.2:x=iw/2-iw*0.1:y=ih-ih*0.2[lt];[base1]crop=w=iw*0.2:h=ih*0.2:x=iw/2-iw*0.1:y=0[lb];[base2]crop=w=iw*0.1:h=ih*0.2:x=iw-iw*0.1:y=ih-ih*0.2[rtl];[base3]crop=w=iw*0.1:h=ih*0.2:x=iw-iw*0.1:y=0[rbl];[base4]crop=w=iw*0.1:h=ih*0.2:x=0:y=ih-ih*0.2[rtr];[base5]crop=w=iw*0.1:h=ih*0.2:x=0:y=0[rbr];[lt][lb]vstack=inputs=2[lhalf];[rtl][rtr]hstack=inputs=2[rtop];[rbl][rbr]hstack=inputs=2[rbottom];[rtop][rbottom]vstack=inputs=2[rhalf];[lhalf][rhalf]hstack=inputs=2[s_mask];[s_mask]split=2[s_mask1][s_mask2];[s_mask2][mask_outher]alphamerge,split=2[masked_alpha1][masked_alpha2];[s_mask1]scale=iw/0.4:ih/0.4:flags=neighbor,format=gray8[fullmask];[scaled]scale=iw:ih[scaledf];[mask][fullmask]blend=all_mode=and[fullmask2];[scaledf][fullmask2]alphamerge[vamasked];[black_scaled][vamasked]overlay=format=auto[outv];[masked_alpha1]crop=iw/2:ih:0:0,split=2[masked_alpha_l1][masked_alpha_l2]; [masked_alpha2]crop=iw/2:ih:iw/2:0,split=4[masked_alpha_r1][masked_alpha_r2][masked_alpha_r3][masked_alpha_r4]; [outv][masked_alpha_l1]overlay=W*0.5-w*0.5:-0.5*h[out_lt];[out_lt][masked_alpha_l2]overlay=W*0.5-w*0.5:H-0.5*h[out_tb]; [out_tb][masked_alpha_r1]overlay=0-w*0.5:-0.5*h[out_l_lt];[out_l_lt][masked_alpha_r2]overlay=0-w*0.5:H-0.5*h[out_tb_ltb]; [out_tb_ltb][masked_alpha_r3]overlay=W-w*0.5:-0.5*h[out_r_lt];[out_r_lt][masked_alpha_r4]overlay=W-w*0.5:H-0.5*h[outv2]" -map "[outv2]" -map "0:a?" -c:a copy "$1-nogb.mp4"
+
